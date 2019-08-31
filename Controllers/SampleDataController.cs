b8e72855b95bdee4fa630cfd8a5952b6a8fb4809
Administrator <Administrator@U9SERVER>
2019-08-31 10:11:38 +0800

    添加项目文件。

---
+++ Controllers/SampleDataController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace myapp_angular.Controllers
+{
+    [Route("api/[controller]")]
+    public class SampleDataController : Controller
+    {
+        private static string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [HttpGet("[action]")]
+        public IEnumerable<WeatherForecast> WeatherForecasts()
+        {
+            var rng = new Random();
+            HttpContext.Response.Cookies.Append("name", "yuchengren");
+            HttpContext.Response.Cookies.Append("name1", "yuchengren");
+            HttpContext.Response.Cookies.Append("name2", "yuchengren");
+            HttpContext.Session.SetString("AdventureWorks.Session","dddd");
+            HttpContext.Session.GetString("AdventureWorks.Session");
+            var cookie = HttpContext.Request.Cookies;
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                DateFormatted = DateTime.Now.AddDays(index).ToString("d"),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            });
+        }
+
+        public class WeatherForecast
+        {
+            public string DateFormatted { get; set; }
+            public int TemperatureC { get; set; }
+            public string Summary { get; set; }
+
+            public int TemperatureF
+            {
+                get
+                {
+                    return 32 + (int)(TemperatureC / 0.5556);
+                }
+            }
+        }
+    }
+}
